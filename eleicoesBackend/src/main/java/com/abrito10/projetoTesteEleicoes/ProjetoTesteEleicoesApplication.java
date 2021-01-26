e45360b837fb1d24484775190098594e42cb4936
adilson <abrito10@gmail.com>
2021-01-26 18:13:16 -0300

    Projeto Teste - Eleicoes criado

---
+++ eleicoesBackend/src/main/java/com/abrito10/projetoTesteEleicoes/ProjetoTesteEleicoesApplication.java
@@ -0,0 +1,13 @@
+package com.abrito10.projetoTesteEleicoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTesteEleicoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteEleicoesApplication.class, args);
+	}
+
+}
---
+++ eleicoesBackend/src/test/java/com/abrito10/projetoTesteEleicoes/ProjetoTesteEleicoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrito10.projetoTesteEleicoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTesteEleicoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
